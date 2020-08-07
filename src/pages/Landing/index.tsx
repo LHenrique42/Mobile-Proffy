156f513f04b25b2775dcdffd9cfc497b751553b8
Henrique <henriquegundes@outlook.com>
2020-08-06 23:28:26 -0300

    Intern navigation

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -18,6 +18,10 @@ function Landing() {
         navigation.navigate('GiveClasses');
     }
 
+    function handleNavigateToStudyPages() {
+        navigation.navigate('Study');
+    }
+
     return (
         <View style={styles.container}>
             <Image source={landingImg} style={styles.banner}/>
@@ -28,7 +32,10 @@ function Landing() {
             </Text>
 
             <View style={styles.buttonsContainer} >
-                <RectButton style={[styles.button, styles.buttonPrimary]}>
+                <RectButton 
+                    style={[styles.button, styles.buttonPrimary]}
+                    onPress={handleNavigateToStudyPages}
+                >
                     <Image source={studyIcon}/>
 
                     <Text style={styles.buttonText}>Estudar</Text>
